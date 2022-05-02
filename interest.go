1fe432a866a4819b28e47375e58636ac8ac07d1f
Nick Glynn <n.s.glynn@gmail.com>
2022-05-02 22:25:22 +1000

    Do some fun LoC loans

---
+++ interest.go
@@ -0,0 +1,159 @@
+package main
+
+import (
+	"log"
+
+	"github.com/shopspring/decimal"
+)
+
+// SimpleInterest is used for non-compounding interest
+// It doesn’t take into account the effect of compounding.
+// In many cases, interest compounds with each designated period of a loan,
+// but in the case of simple interest, it does not.
+// The calculation of simple interest is equal to the principal amount multiplied by the
+// interest rate, multiplied by the number of periods.
+//
+// Do NOT call this using integer math inline, i.e.
+// SimpleInterest(..., 0.10, 3/12 )
+// will fail due to Go's conventions
+func SimpleInterest(principal decimal.Decimal, ratePct float64, period float64) decimal.Decimal {
+	return decimal.NewFromFloat(principal.InexactFloat64() * ratePct * period)
+}
+
+type Action int
+
+const (
+	Credit Action = iota
+	Debit
+)
+
+func (a Action) String() string {
+	if a == Credit {
+		return "Credit"
+	}
+	return "Debit"
+}
+
+type transaction struct {
+	day       int
+	amount    decimal.Decimal
+	direction Action
+	// -- Unused Below
+	drawDownFee   decimal.Decimal
+	drawDownPct   decimal.Decimal
+	billingPeriod int
+	daysInYear    int
+}
+
+type Loan struct {
+	creditLimit  decimal.Decimal
+	apr          float64
+	transactions []transaction
+}
+
+func NewLoan(creditLimit decimal.Decimal, APR float64) Loan {
+	return Loan{
+		creditLimit:  creditLimit,
+		apr:          APR,
+		transactions: []transaction{},
+	}
+}
+
+func (l *Loan) Draw(amount decimal.Decimal, day int) {
+	l.transactions = append(l.transactions, transaction{
+		day:       day,
+		amount:    amount,
+		direction: Debit,
+	})
+}
+
+func (l *Loan) Pay(amount decimal.Decimal, day int) {
+	l.transactions = append(l.transactions, transaction{
+		day:       day,
+		amount:    amount,
+		direction: Credit,
+	})
+}
+
+func (l *Loan) getTransactions(day int) []transaction {
+	txns := []transaction{}
+	for i := range l.transactions {
+		if l.transactions[i].day == day {
+			txns = append(txns, l.transactions[i])
+		}
+	}
+	return txns
+}
+
+func (l *Loan) dumpTransactions(day int) {
+	balance := decimal.Zero
+	var accInterest decimal.Decimal
+	log.Println("Day\t Action \t Amount")
+	for i := 0; i <= day; i++ {
+		txns := l.getTransactions(i)
+		for j := range txns {
+			switch txns[j].direction {
+			case Credit:
+				balance = balance.Sub(txns[j].amount.Abs())
+				log.Printf("%d \t %v \t %v", i, Credit, txns[j].amount)
+			case Debit:
+				balance = balance.Sub(txns[j].amount.Abs())
+				log.Printf("%d \t %v \t (%v)", i, Debit, txns[j].amount)
+			}
+		}
+		if balance.GreaterThan(decimal.Zero) {
+			// log.Println("Carried interest on day", i)
+			dailyAPR := (l.apr / 365.00)
+			accInterest = accInterest.Add(balance.Mul(decimal.NewFromFloat(dailyAPR)))
+		}
+		log.Println("____________________________________________")
+		log.Println("Balance at end of day", i, "", balance)
+		log.Println("Interest owed", accInterest)
+	}
+}
+
+func (l *Loan) GetBalance(day int) decimal.Decimal {
+	balance := decimal.Zero
+	for i := 0; i <= day; i++ {
+		txns := l.getTransactions(i)
+		for j := range txns {
+			switch txns[j].direction {
+			case Credit:
+				balance = balance.Sub(txns[j].amount.Abs())
+			case Debit:
+				balance = balance.Add(txns[j].amount.Abs())
+			}
+		}
+	}
+	return l.creditLimit.Sub(balance)
+}
+
+func (l *Loan) GetLimitAndBalance(day int) (decimal.Decimal, decimal.Decimal) {
+	return l.creditLimit.Sub(l.GetBalance(day)), l.GetBalance(day)
+}
+
+func (l *Loan) GetInterestOwed(day int) decimal.Decimal {
+	var accInterest decimal.Decimal
+
+	// Starting at day 1 of billing period
+	// We zip through and accumulate interest for the days we hold a balance
+	balance := decimal.Zero
+	for i := 0; i <= day; i++ {
+		txns := l.getTransactions(i)
+		for j := range txns {
+			switch txns[j].direction {
+			case Credit:
+				balance = balance.Sub(txns[j].amount.Abs())
+			case Debit:
+				balance = balance.Add(txns[j].amount.Abs())
+			}
+		}
+		if balance.GreaterThan(decimal.Zero) {
+			// log.Println("Carried interest on day", i)
+			dailyAPR := (l.apr / 365.00)
+			accInterest = accInterest.Add(balance.Mul(decimal.NewFromFloat(dailyAPR)))
+		}
+	}
+
+	return accInterest.RoundBank(2)
+}
--- main.go
+++ main.go
@@ -7,6 +7,65 @@ import (
 )
 
 func main() {
+	testInterest()
+}
+
+func testInterest() {
+
+	// Create an implementation of the following:
+
+	// A line of credit product. This is like a credit card except theres no card. It should work like this:
+
+	// Have a built in APR and credit limit
+	// Be able to draw ( take out money ) and make payments.
+	// Keep track of principal balance and interest on the line of credit
+	// APR Calculation based on the outstanding principal balance over real number of days.
+	// Interest is not compounded, so it is only charged on outstanding principal.
+	// Keep track of transactions such as payments and draws on the line and when they occured.
+	// 30 day payment periods. Basically what this means is that interest will not be charged until
+	// the closing of a 30 day payment period. However, when it is charged, it should still be based
+	// on the principal balance over actual number of days outstanding during the period, not just
+	// ending principal balance.
+	// Couple of Scenarios how it would play out:
+
+	// Scenario 1:
+	// Someone creates a line of credit for $1000 and 35% APR.
+	// They draws $500 on day one so their remaining credit limit is $500 and their balance is $500.
+	// They keep the money drawn for 30 days. They should owe $500 * 0.35 / 365 * 30 = 14.38$ worth
+	// of interest on day 30.
+	// Total payoff amount would be $514.38
+
+	// Scenario 2:
+	// Someone creates a line of credit for $1000 and 35% APR.
+	// They draw $500 on day one so their remaining credit limit is $500 and their balance is $500.
+	// They pay back $200 on day 15 and then draws another 100$ on day 25.
+	// Their total owed interest on day 30 should be:
+	// 500 * 0.35 / 365 * 15 + 300 * 0.35 / 365 * 10 + 400 * 0.35 / 365 * 5 which is 11.99.
+	// Total payment should be $411.99.
+
+	log.Println("Scenario 1")
+	log.Println("===========")
+	loan := NewLoan(decimal.NewFromInt(1000), 0.35)
+	loan.Draw(decimal.NewFromInt(500), 1)
+	loan.dumpTransactions(1)
+	log.Println()
+	log.Println("\tDay 30 dues", loan.GetInterestOwed(30))
+	log.Println()
+
+	log.Println("Scenario 2")
+	log.Println("===========")
+	loan = NewLoan(decimal.NewFromInt(1000), 0.35)
+	loan.Draw(decimal.NewFromInt(500), 1)
+	loan.Pay(decimal.NewFromInt(200), 15)
+	loan.Draw(decimal.NewFromInt(100), 25)
+	loan.dumpTransactions(0)
+	log.Println()
+	log.Println("\tDay 30 dues", loan.GetInterestOwed(30))
+	log.Println()
+
+}
+
+func testFinance() {
 	log.Println(Pmt(.07, 5, decimal.NewFromInt(100000), DueAtEnd))
 	//
 	log.Println(PmtWithFutureValue(.07/12, 5*12, decimal.NewFromInt(100000), decimal.Zero, DueAtEnd))
